e4984548792999826d9c527a364148a3d5a31ebc
attabadukwabena <attabadukwabena.gmail.com>
2023-02-07 20:13:10 +0000

    Add /profile endpoint to profiles/urls.py

---
+++ profiles/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+
+from profiles.views import ProfileViewSet
+
+
+router = routers.DefaultRouter()
+
+router.register('profile', ProfileViewSet, basename='profile')
+
+urlpatterns = router.urls

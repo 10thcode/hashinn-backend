06ff103a923d2ea276bae45fb47206d1eb4942f5
attabadukwabena <attabadukwabena.gmail.com>
2023-02-07 20:22:26 +0000

    Change HyperlinkedSerializer to HyperlinkedModelSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 from profiles.models import Profile
 
 
-class UserSerializer(serializers.HyperlinkedSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username']
 
 
-class ProfileSerializer(serializers.HyperlinkedSerializer):
-    user = UserSerializer(source='user')
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
 
     class Meta:
         model = Profile
